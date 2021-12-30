e30acc64280edea8f3029f7f09b4f77ed15d9f76
Erick C <cherick04@hotmail.com>
2021-12-30 18:05:18 -0300

    Deploying NFT creation

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,6 +1,16 @@
-from scripts.helpful_scripts import get_account, SimpleCollectable
+from scripts.helpful_scripts import get_account
+from brownie import SimpleCollectable
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
 def main():
     account = get_account()
     simple_collectable = SimpleCollectable.deploy({"from": account})
+    tx = simple_collectable.createCollectable(sample_token_uri, {"from": account})
+    tx.wait(1)
+    print(
+        f"Great! You can view your NFT at {OPENSEA_URL.format(simple_collectable.address, simple_collectable.tokenCounter()-1)}"
+    )
+    print("Please wait up to 20 minutes and hit the refresh metadata button")
