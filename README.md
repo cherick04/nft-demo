d2ee45bf4e1a7af354296c05411330496dd49789
Erick C <cherick04@hotmail.com>
2022-01-03 18:03:45 -0300

    Added AdvancedCollectable solidity file

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Need to upload an image to IPFS ourselves
+2. Why is IPFS decentralized?
+3. Anyone can mint and NFT here - need to make it verifiably scarce and random
---
+++ contracts/AdvancedCollectable.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract AdvancedCollectable is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyhash;
+    uint256 public fee;
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+    mapping(uint256 => Breed) public tokenIdToBreed;
+    mapping(bytes32 => address) public requestIdToSender;
+    // Good practice to emit an event after updating a mapping
+    event requestedCollectable(bytes32 indexed requestId, address requester);
+    event breedAssigned(uint256 indexed tokenId, Breed breed);
+
+    constructor(
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _keyhash,
+        uint256 _fee
+    )
+        public
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+        ERC721("Doggie", "DOG")
+    {
+        tokenCounter = 0;
+        keyhash = _keyhash;
+        fee = _fee;
+    }
+
+    function createCollectable() public returns (bytes32) {
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        requestIdToSender[requestId] = msg.sender;
+        emit requestedCollectable(requestId, msg.sender);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
+        internal
+        override
+    {
+        Breed breed = Breed(randomNumber % 3);
+        uint256 newTokenId = tokenCounter;
+        tokenIdToBreed[newTokenId] = breed;
+        emit breedAssigned(newTokenId, breed);
+        address owner = requestIdToSender[requestId];
+        _safeMint(owner, newTokenId); // msg.sender needs to be the caller of the collectable (owner)
+        tokenCounter += 1;
+    }
+
+    function setTokenURI(uint256 tokenId, string memory tokenURI) public {
+        // _isApprovedOrOwner and _msgSender() are imported from OpenZepplin
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not owner nor approved"
+        );
+        _setTokenURI(tokenId, tokenURI);
+    }
+}
